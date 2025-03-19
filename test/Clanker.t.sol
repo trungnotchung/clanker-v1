b61aca126fc3703575bc86f0bb66e73e8e17a2e2
trungnotchung <trung09072004@gmail.com>
2025-03-20 00:42:44 +0700

    update tests

--- test/Clanker.t.sol
+++ test/Clanker.t.sol
@@ -14,7 +14,7 @@ contract ClankerTest is Test {
         0x2626664c2603336E57B271c5C0b26F421741e481;
     address constant OWNER = 0x7372d36388E5e7d2cf7B3B9b4dB106D442F9a1a7;
     address constant CLANKER_ADDRESS =
-        0x59979986B39245FbE1A4219B54848e542B5f2355;
+        0x4672EBd5296870056f8E825e8f1087e20AA7F2BA;
     address constant CLANKER_TEAM_ADDRESS =
         0x7372d36388E5e7d2cf7B3B9b4dB106D442F9a1a7;
     uint256 constant CLANKER_TEAM_REWARD = 60;
@@ -23,48 +23,25 @@ contract ClankerTest is Test {
     LpLockerv2 lpLocker;
 
     function setUp() public {
-        vm.startBroadcast(OWNER);
-        lpLocker = new LpLockerv2{
-            salt: keccak256(
-                abi.encode("0x7372d36388E5e7d2cf7B3B9b4dB106D442F9a1a7")
-            )
-        }(
-            CLANKER_ADDRESS,
-            POSITION_MANAGER_ADDRESS,
-            CLANKER_TEAM_ADDRESS,
-            CLANKER_TEAM_REWARD
-        );
-
-        clanker = new Clanker{
-            salt: keccak256(
-                abi.encode("0x7372d36388E5e7d2cf7B3B9b4dB106D442F9a1a7")
-            )
-        }(
-            address(lpLocker),
-            UNISWAPV3_ADDRESS,
-            POSITION_MANAGER_ADDRESS,
-            SWAP_ROUTER_ADDRESS,
-            OWNER
-        );
-        lpLocker.updateClankerFactory(address(clanker));
-        console.log("Clanker address: ", address(clanker));
-        clanker.toggleAllowedPairedToken(
-            0x4200000000000000000000000000000000000006,
-            true
-        );
+        clanker = Clanker(0x4672EBd5296870056f8E825e8f1087e20AA7F2BA);
+        lpLocker = LpLockerv2(0x3634E4810a0ed0f649079A9d479b2CbCC312d7BF);
     }
 
     function testDeploy() public {
+        vm.startBroadcast(OWNER);
+        address owner = clanker.owner();
+        console.log("owner", owner);
+        console.log("factory", lpLocker._factory());
         (, uint256 positionId) = clanker.deployToken(
             "Test Token",
             "Test Symbol",
-            1000000000,
+            1000000000000000000,
             10000,
-            0xfd64a4064216e669183f9fd239f674557f855b54c9a7fe22761517c87123b69b,
+            0x3dd19da992fefacaad68bbdcb7f337d1c46239a77656e75bb6eb6f47279d3491,
             OWNER,
             0,
             "haha",
-            "0x7949590b65138ed6736e5e5acc1881f8b9ccc559",
+            "clanker",
             Clanker.PoolConfig({
                 tick: -230400,
                 pairedToken: 0x4200000000000000000000000000000000000006,
@@ -72,6 +49,7 @@ contract ClankerTest is Test {
             })
         );
 
-        console.log("Position ID: ", positionId);
+        // console.log("Position ID: ", positionId);
+        vm.stopBroadcast();
     }
 }
